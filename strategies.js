c3a519ca2a924d6a4c758daaf7d329b4b02aac7b
belawastaken <heischkel.bela@gmail.com>
2026-07-28 02:51:21 +0200

    fixed i4 not highlighting

--- strategies.js
+++ strategies.js
@@ -2,7 +2,7 @@ const strategies = {
     m7_guides: {
         roles: {
             "Archer": [ "s1term4", "s1term3", "s2term4", "s2levRight", "s3term4", "s3levRight", "s3levLeft", "s4term2" ],
-            "Bers": [ "i4", "s2term5", "s2term3", "s2levRight", "s3term3", "s3dev", "s4term3", "s4levLeft", "s4levRight" ],
+            "Bers": [ "s4dev", "s2term5", "s2term3", "s2levRight", "s3term3", "s3dev", "s4term3", "s4levLeft", "s4levRight" ],
             "Mage": [ "s1levLeft", "s1levRight", "s2eeHigh", "s2dev", "s2term2", "core" ],
             "Tank": [ "s1term2", "s1term1", "s2term1", "s2term3", "s2levRight", "s3term1", "s3levRight", "s3levLeft", "s4term1" ],
             "Healer": [ "s1dev", "s2dev", "s2levLeft", "s3eeLow", "s3term2", "s3dev","s4term4", "s4levRight", "s4levLeft" ]
